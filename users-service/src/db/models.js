29b15092597d03159d8cc78f82cd29ca5f49b63a
ductoanthanh <arsenaltoan10@gmail.com>
2020-02-06 15:46:22 +0200

    Add user session model in user service

--- users-service/src/db/models.js
+++ users-service/src/db/models.js
@@ -28,3 +28,32 @@ User.init(
     sequelize
   }
 );
+
+export class UserSession extends Model {}
+UserSession.init(
+  {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID
+    },
+    userId: {
+      allowNull: false,
+      references: {
+        key: "id",
+        model: "users"
+      },
+      type: DataTypes.UUID
+    },
+    expiresAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  },
+  {
+    modelName: "userSessions",
+    paranoid: false,
+    sequelize,
+    updatedAt: false
+  }
+);
