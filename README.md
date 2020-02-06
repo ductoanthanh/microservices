44294298aeff75952062d240c491371dd3195b94
Duc Toan Thanh <toanth@metropolia.fi>
2020-02-07 01:45:30 +0200

    Setup test EC2 from Terraform

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 by default GraphQL playground do not show cookie --> enable by Settings -> "request.credentials": "include"
 
 give Terraform admin right in IAM
+terraform init
+terraform apply - yes
+terraform destroy
---
+++ terraform/test-server.tf
@@ -0,0 +1,6 @@
+module "test-server" {
+  source   = "./node-server"
+  ami-id   = "ami-0b8b10b5bf11f3a22" # match with ap-southeast-2 region
+  key-pair = aws_key_pair.microservices-key.key_name
+  name     = "Test Server"
+}
